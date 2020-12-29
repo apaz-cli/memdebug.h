431587d82099aa0b0160387e749fc3b6174b7bc2
apaz-cli <aarpazdera@gmail.com>
2020-12-29 15:30:56 -0600

    Added support for ANSI terminal escape sequences

--- memdebug.h
+++ memdebug.h
@@ -1,3 +1,5 @@
+
+
 /***********/
 /* Mutexes */
 /***********/
@@ -37,8 +39,44 @@ static inline int mutex_lock(mutex_t* mutex) { return pthread_mutex_lock(mutex);
 static inline int mutex_unlock(mutex_t* mutex) { return pthread_mutex_unlock(mutex); }
 static inline int mutex_destroy(mutex_t* mutex) { return pthread_mutex_destroy(mutex); }
 #endif
-
 #endif  // End mutex include guard
+
+/*******************************/
+/* Pretty ANSI Terminal Colors */
+/*******************************/
+
+// #define ANSI_TERMINAL 0
+#ifdef _WIN32
+#define ANSI_TERMINAL 0
+#else
+#define ANSI_TERMINAL 1
+#endif
+
+#if ANSI_TERMINAL
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_YELLOW "\x1b[33m"
+#define ANSI_COLOR_BLUE "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN "\x1b[36m"
+#define ANSI_COLOR_RESET "\x1b[0m"
+#else
+#define ANSI_COLOR_RED ""
+#define ANSI_COLOR_GREEN ""
+#define ANSI_COLOR_YELLOW ""
+#define ANSI_COLOR_BLUE ""
+#define ANSI_COLOR_MAGENTA ""
+#define ANSI_COLOR_CYAN ""
+#define ANSI_COLOR_RESET ""
+#endif
+
+#define ANSI_COLOR_HEAD ANSI_COLOR_RED
+#define ANSI_COLOR_PNIC ANSI_COLOR_RED
+#define ANSI_COLOR_PNTR ANSI_COLOR_MAGENTA
+#define ANSI_COLOR_BYTE ANSI_COLOR_BLUE
+#define ANSI_COLOR_FILE ANSI_COLOR_GREEN
+#define ANSI_COLOR_FUNC ANSI_COLOR_YELLOW
+#define ANSI_COLOR_LINE ANSI_COLOR_CYAN
 
 /************/
 /* MEMDEBUG */
@@ -205,8 +243,10 @@ alloc_add(MemAlloc alloc) {
 // returns the pointer, or NULL if not found.
 static inline bool
 alloc_remove(void* ptr) {
-    if (!memallocs_initialized)
+    if (!memallocs_initialized) {
         memallocs_init();
+        return false;
+    }
 
     MapMember* previous = NULL;
     MapMember* bucket = (MapMember*)allocs + ptr_hash(ptr);
@@ -255,7 +295,13 @@ alloc_remove(void* ptr) {
 
 static inline void
 mempanic(void* badptr, const char* message, size_t line, const char* func, const char* file) {
-    printf("\nMEMORY PANIC: %s\nPointer: %p\nOn line: %zu\nIn function: %s\nIn file: %s\nAborted.\n", message, badptr, line, func, file);
+    printf(ANSI_COLOR_PNIC "\nMEMORY PANIC: %s\n" ANSI_COLOR_RESET
+               ANSI_COLOR_PNTR "Pointer: %p\n" ANSI_COLOR_RESET
+                   ANSI_COLOR_LINE "On line: %zu\n" ANSI_COLOR_RESET
+                       ANSI_COLOR_FUNC "In function: %s()\n" ANSI_COLOR_RESET
+                           ANSI_COLOR_FILE "In file: %s\n" ANSI_COLOR_RESET
+                               ANSI_COLOR_PNIC "Aborted.\n" ANSI_COLOR_RESET,
+           message, badptr, line, func, file);
     fflush(stdout);
     exit(MEMPANIC_EXIT_STATUS);
 }
@@ -263,18 +309,66 @@ mempanic(void* badptr, const char* message, size_t line, const char* func, const
 static inline void
 OOM(size_t line, const char* func, const char* file, size_t num_bytes) {
     if (strcmp(file, "memdebug.h") == 0) {
-        printf(
-            "\nIronically, this program has run out of memory keeping track of or printing memory allocations."
-            "\nThe error did not happen inside your program.");
+        printf(ANSI_COLOR_PNIC
+               "\nIronically, this program has run out of memory keeping track of or printing memory allocations."
+               "\nThe error did not happen inside your program, but it was likely caused by it because you allocated too much memory." ANSI_COLOR_RESET);
     }
-    printf("\nOut of memory on line %zu in %s() in file: %s.\nCould not allocate %zu bytes.\nDumping heap:\n", line, func, file, num_bytes);
+    printf(
+        ANSI_COLOR_PNIC "\n*****************"
+                        "\n* Out of Memory *" 
+                        "\n*****************\n"ANSI_COLOR_RESET
+            ANSI_COLOR_FILE "In file: %s\n" ANSI_COLOR_RESET
+                ANSI_COLOR_FUNC "In function: %s()\n" ANSI_COLOR_RESET
+                    ANSI_COLOR_LINE "On line: %zu\n" ANSI_COLOR_RESET
+                        ANSI_COLOR_BYTE "Could not allocate %zu bytes.\n" ANSI_COLOR_RESET,
+        file, func, line, num_bytes);
 
     low_mem_print_heap();
     exit(OOM_EXIT_STATUS);
 }
 
+/**************************/
+/* Print Helper Functions */
+/**************************/
+
+static inline void
+print_alloc_summary(size_t total_ptrs_at_location, size_t total_bytes_at_location, char* location_file, char* location_func, size_t location_line) {
+    if (total_ptrs_at_location == 1) {
+        printf(
+            ANSI_COLOR_PNTR "%zu pointer has been allocated" ANSI_COLOR_RESET
+                ANSI_COLOR_BYTE " totalling %zu bytes" ANSI_COLOR_RESET
+                    ANSI_COLOR_FILE " in file: %s" ANSI_COLOR_RESET
+                        ANSI_COLOR_FUNC " in function: %s" ANSI_COLOR_RESET
+                            ANSI_COLOR_LINE " on line: %zu.\n" ANSI_COLOR_RESET,
+            total_ptrs_at_location, total_bytes_at_location, location_file, location_func, location_line);
+    } else {
+        printf(
+            ANSI_COLOR_PNTR "%zu pointers have been allocated" ANSI_COLOR_RESET
+                ANSI_COLOR_BYTE " totalling %zu bytes" ANSI_COLOR_RESET
+                    ANSI_COLOR_FILE " in file: %s" ANSI_COLOR_RESET
+                        ANSI_COLOR_FUNC " in function: %s" ANSI_COLOR_RESET
+                            ANSI_COLOR_LINE " on line: %zu.\n" ANSI_COLOR_RESET,
+            total_ptrs_at_location, total_bytes_at_location, location_file, location_func, location_line);
+    }
+}
+
+static inline void
+print_heap_summary_totals(size_t total_allocated, size_t num_allocs) {
+    printf(
+        "\nTotal Heap size in bytes: %zu"
+        "\nTotal number of heap allocations: %zu"
+        "\n\n\n",
+        total_allocated, num_allocs);
+    fflush(stdout);
+}
+
+static inline void
+print_heap_dump_header() {
+    printf(ANSI_COLOR_HEAD "\n*************\n* HEAP DUMP *\n*************\n" ANSI_COLOR_RESET);
+}
+
 /**********************/
-/* Externally visible */
+/* Externally Visible */
 /**********************/
 
 // Print all of the memory allocations of this program.
@@ -306,14 +400,40 @@ void print_heap() {
     // Sort the buffer
     sort_memallocs(all_allocs, allocs_idx);
 
-    // For now, just print the buffer.
-    printf("\n*************\n* HEAP DUMP *\n*************\n");
-    for (size_t i = 0; i < allocs_idx; i++) {
-        MemAlloc alloc = all_allocs[i];
-        printf("Heap ptr: %p of size: %zu Allocated in file: %s On line: %zu\n",
-               alloc.ptr, alloc.size, alloc.file, alloc.line);
+    // Print the formatted results
+    print_heap_dump_header();
+    if (allocs_idx) {
+        char* location_file = NULL;
+        char* location_func;
+        size_t location_line;
+
+        size_t total_bytes_at_location;
+        size_t total_ptrs_at_location;
+
+        for (size_t i = 0; i < allocs_idx; i++) {
+            MemAlloc alloc = all_allocs[i];
+
+            // If the current allocation is not the same as the last, print the summary of that location in the code.
+            if ((alloc.line != location_line) || (alloc.func != location_func) || (alloc.file != location_file)) {
+                if (location_file != NULL) {
+                    print_alloc_summary(total_ptrs_at_location, total_bytes_at_location, location_file, location_func, location_line);
+                }
+
+                location_file = (char*)alloc.file;
+                location_func = (char*)alloc.func;
+                location_line = alloc.line;
+                total_bytes_at_location = alloc.size;
+                total_ptrs_at_location = 1;
+            } else {
+                total_bytes_at_location += alloc.size;
+                total_ptrs_at_location++;
+            }
+        }
+
+        print_alloc_summary(total_ptrs_at_location, total_bytes_at_location, location_file, location_func, location_line);
     }
-    printf("\nTotal Heap size in bytes: %zu, number of items: %zu\n\n\n", total_allocated, num_allocs);
+
+    print_heap_summary_totals(total_allocated, num_allocs);
 
     free(all_allocs);
 }
@@ -329,13 +449,17 @@ void low_mem_print_heap() {
         memallocs_init();
 
     // For each bucket, traverse over each and print all the allocations
-    printf("\n*************\n* HEAP DUMP *\n*************\n");
+    print_heap_dump_header();
     for (size_t i = 0; i < MAP_BUF_SIZE; i++) {
         MapMember* bucket = (MapMember*)(allocs + i);
         while (bucket->alloc.ptr != NULL) {
             MemAlloc alloc = bucket->alloc;
-            printf("Heap ptr: %p of size: %zu Allocated in file: %s On line: %zu\n",
-                   alloc.ptr, alloc.size, alloc.file, alloc.line);
+            printf(
+                ANSI_COLOR_PNTR "Heap ptr: %p" ANSI_COLOR_RESET
+                    ANSI_COLOR_BYTE " of size: %zu" ANSI_COLOR_RESET
+                        ANSI_COLOR_FILE " Allocated in file: %s" ANSI_COLOR_RESET
+                            ANSI_COLOR_LINE " On line: %zu\n" ANSI_COLOR_RESET,
+                alloc.ptr, alloc.size, alloc.file, alloc.line);
             total_allocated += alloc.size;
 
             if (bucket->next) {
@@ -348,8 +472,7 @@ void low_mem_print_heap() {
 
     MEMDEBUG_UNLOCK_MUTEX;
 
-    printf("\nTotal Heap size in bytes: %zu, number of items: %zu\n\n\n", total_allocated, num_allocs);
-    fflush(stdout);
+    print_heap_summary_totals(total_allocated, num_allocs);
 }
 
 /*********************************************/
@@ -363,7 +486,15 @@ void* memdebug_malloc(size_t n, size_t line, const char* func, const char* file)
 
 #if PRINT_MEMALLOCS
     // Print message
-    printf("malloc(%zu) -> %p on line %zu in %s() in %s.\n", n, ptr, line, func, file);
+    printf(ANSI_COLOR_FUNC "malloc(" ANSI_COLOR_RESET
+               ANSI_COLOR_BYTE "%zu" ANSI_COLOR_RESET
+                   ANSI_COLOR_FUNC ")" ANSI_COLOR_RESET
+                           " -> " ANSI_COLOR_PNTR "%p" ANSI_COLOR_RESET
+                           " on line " ANSI_COLOR_LINE "%zu" ANSI_COLOR_RESET
+                           " in " ANSI_COLOR_FUNC "%s()" ANSI_COLOR_RESET
+                           " in " ANSI_COLOR_FILE "%s" ANSI_COLOR_RESET
+                           ".\n",
+           n, ptr, line, func, file);
     fflush(stdout);
 #endif
 
@@ -396,7 +527,18 @@ void* memdebug_realloc(void* ptr, size_t n, size_t line, const char* func, const
 
 #if PRINT_MEMALLOCS
     // Print message
-    printf("realloc(%p, %zu) -> %p on line %zu in %s() in %s.\n", ptr, n, newptr, line, func, file);
+    printf(
+        ANSI_COLOR_FUNC "realloc(" ANSI_COLOR_RESET
+            ANSI_COLOR_PNTR "%p" ANSI_COLOR_RESET
+                ANSI_COLOR_FUNC ", " ANSI_COLOR_RESET
+                    ANSI_COLOR_BYTE "%zu" ANSI_COLOR_RESET
+                        ANSI_COLOR_FUNC ")" ANSI_COLOR_RESET
+                        " -> " ANSI_COLOR_PNTR "%p" ANSI_COLOR_RESET
+                        " on line " ANSI_COLOR_LINE "%zu" ANSI_COLOR_RESET
+                        " in " ANSI_COLOR_FUNC "%s()" ANSI_COLOR_RESET
+                        " in " ANSI_COLOR_FUNC "%s" ANSI_COLOR_RESET
+                        ".\n",
+        ptr, n, newptr, line, func, file);
     fflush(stdout);
 #endif
 
@@ -430,7 +572,15 @@ void memdebug_free(void* ptr, size_t line, const char* func, const char* file) {
 
 #if PRINT_MEMALLOCS
     // Print message
-    printf("free(%p) on line %zu in %s() in %s.\n", ptr, line, func, file);
+    printf(
+        ANSI_COLOR_FUNC "free(" ANSI_COLOR_RESET
+            ANSI_COLOR_PNTR "%p" ANSI_COLOR_RESET
+                ANSI_COLOR_FUNC ")" ANSI_COLOR_RESET
+                        " on line " ANSI_COLOR_LINE "%zu" ANSI_COLOR_RESET
+                        " in " ANSI_COLOR_FUNC "%s()" ANSI_COLOR_RESET
+                        " in " ANSI_COLOR_FILE "%s" ANSI_COLOR_RESET
+                        ".\n",
+        ptr, line, func, file);
     fflush(stdout);
 #endif
 }
